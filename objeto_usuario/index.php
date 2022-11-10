961033ecbad929699103952cb2937173b12827d3
dax <arnaualbert2003@gmail.com>
2022-11-10 19:38:27 +0100

    act

---
+++ objeto_usuario/index.php
@@ -0,0 +1,42 @@
+<?php session_start();
+include 'lib/usuario.php';
+if(isset($_POST['submit'])){
+    
+
+    // reb la variable user del formulari
+    $user = \filter_input(\INPUT_POST, 'user');
+    $user = \filter_var($user);
+
+    // reb la variable pass del formulare
+    $pass = \filter_input(\INPUT_POST, 'pass');
+    $pass = \filter_var($pass);
+
+
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EJERCICIO 1</title>
+</head>
+<body>
+<form action="<?php echo \htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+    <div>
+        <label for="">Username</label>
+        <div>
+            <input type="text" >
+        </div>
+    </div>
+    <div>
+        <label for="">Password</label>
+        <div>
+            <input type="text" >
+        </div>
+    </div>
+</form>
+</body>
+</html>
