4783fcc438b7900da074bb3a3e16476e967a48b6
dax <arnaualbert2003@gmail.com>
2022-11-15 18:11:37 +0100

    act

--- objeto_usuario_dos/model/persist/userpersist.php
+++ objeto_usuario_dos/model/persist/userpersist.php
@@ -1,1 +1,17 @@
 <?php
+class UserFilePersist {
+    private string $filename;
+
+    public function __construct($filename){
+        $this->filename=$filename;
+    }
+
+    public function getfilename(){
+        return $this->$filename;
+    }
+
+    public function setfilename($filename) {
+        $this->filename = $filename;
+    }
+
+}
